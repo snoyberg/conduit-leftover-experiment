914a71ab6f27b6401c2e34f722ff892046c2d4bc
Michael Snoyman <michael@fpcomplete.com>
2013-09-27 15:08:32 +0000

    Leftovers!

--- src/Conduit.hs
+++ src/Conduit.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies, EmptyDataDecls #-}
+{-# LANGUAGE TypeFamilies, EmptyDataDecls, TupleSections #-}
 {-# OPTIONS_GHC -Wall #-}
 module Conduit where
 
@@ -10,7 +10,88 @@ import Control.Monad.Trans.Class
 import Test.Hspec
 import Prelude hiding (take, id, (.))
 import Control.Category
+import Control.Arrow (first)
 import Data.Functor.Identity
+
+leftover :: Monad m => i -> PipeL i o d m ()
+leftover i = PipeL $ const $ return ([i], ())
+
+yieldL :: Monad m => o -> PipeL i o d m (Maybe d)
+yieldL o = PipeL $ \_mosd -> do
+    mres <- yield o
+    case mres of
+        Nothing -> return ([], Nothing)
+        Just (_downstreamleftovers, x) -> return ([], Just x)
+
+awaitL :: Monad m => PipeL i o d m (Maybe i)
+awaitL = PipeL $ const $ liftM (\x -> ([], x)) await
+
+takeL :: Monad m => Int -> PipeL i i d m ()
+takeL 0 = return ()
+takeL i = awaitL >>= maybe (return ()) (\x -> yieldL x >> (takeL $ i - 1))
+
+consumeL :: Monad m => PipeL i o d m [i]
+consumeL =
+    loop id
+  where
+    loop front = awaitL >>= maybe (return $ front []) (\x -> loop (front . (x:)))
+
+sourceListL :: Monad m => [o] -> PipeL i o d m ()
+sourceListL [] = return ()
+sourceListL (o:os) = yieldL o >>= maybe (sourceListL os) (const $ return ())
+
+stopL :: Monad m => PipeL i o r m r
+stopL = PipeL $ \mosd ->
+    case mosd of
+        Just (_, r) -> return ([], r)
+        Nothing -> do
+            (_leftovers, r) <- stop
+            return ([], r)
+
+newtype ComposePipeL m up down = ComposePipeL
+    { runComposePipeL :: PipeL (Stream up) (Stream down) (Result down) m (Result up)
+    }
+
+idL :: Monad m => PipeL i i r m r
+idL = PipeL $ \mosd ->
+        case mosd of
+            Just (os, d) -> return (os, d)
+            Nothing -> idP
+
+
+instance Monad m => Category (ComposePipeL m) where
+    id = ComposePipeL $ PipeL $ \mosd ->
+        case mosd of
+            Just (os, d) -> return (os, d)
+            Nothing -> idP
+    ComposePipeL down . ComposePipeL up = ComposePipeL $ up `pipeL` down
+
+newtype PipeL i o d m r = PipeL
+    { runPipeL :: Maybe ([o], d) -> Pipe i o ([o], d) m ([i], r) -- FIXME return (Maybe d) next to r
+    }
+    
+runPipeLI :: PipeL i o () Identity r -> r
+runPipeLI (PipeL p) = snd $ runIdentity $ runPipe' ([], ()) (p Nothing)
+
+pipeL :: Monad m
+      => PipeL i j b m a
+      -> PipeL j k c m b
+      -> PipeL i k c m a
+pipeL (PipeL up) (PipeL down) = PipeL $ fuse up . down
+
+instance Monad m => Monad (PipeL i o d m) where
+    return = PipeL . const . return . ([], )
+    PipeL f >>= g = PipeL $ \mosd -> do
+        (is, r) <- f mosd
+        let PipeL g' = g r
+        inject is $ g' $ fmap (first $ const []) mosd
+
+inject :: Monad m => [i] -> Pipe i o d m ([i], r) -> Pipe i o d m ([i], r)
+inject [] p = p
+inject is (Pure (is', r)) = Pure (is' ++ is, r)
+inject is (M m) = M (liftM (inject is) m)
+inject (i:is) (Await f) = inject is (f (Just i))
+inject is (Yield f o) = Yield (inject is . f) o
 
 data Pipe i o d m r
     = Pure r
@@ -36,13 +117,17 @@ instance MonadTrans (Pipe i o d) where
 idP :: Monad m => Pipe i i r m r
 idP = Await (Yield (maybe idP Pure))
 
-runPipe :: Monad m
-        => Pipe i o () m r
-        -> m r
-runPipe (Pure r) = return r
-runPipe (M m) = m >>= runPipe
-runPipe (Await f) = runPipe (f Nothing)
-runPipe (Yield f _) = runPipe (f (Just ()))
+runPipe' :: Monad m
+         => d
+         -> Pipe i o d m r
+         -> m r
+runPipe' _ (Pure r) = return r
+runPipe' d (M m) = m >>= runPipe' d
+runPipe' d (Await f) = runPipe' d (f Nothing)
+runPipe' d (Yield f _) = runPipe' d (f (Just d))
+
+runPipe :: Monad m => Pipe i o () m r -> m r
+runPipe = runPipe' ()
 
 (>->) :: Monad m
       => Pipe i j b m a
@@ -53,7 +138,10 @@ up >-> down = fuse (const up) down
 type family Stream a
 type family Result a
 
-newtype ComposePipe m up down = ComposePipe (Maybe (Result down) -> Pipe (Stream up) (Stream down) (Result down) m (Result up))
+newtype ComposePipe m up down = ComposePipe
+    { runComposePipe :: Maybe (Result down)
+                     -> Pipe (Stream up) (Stream down) (Result down) m (Result up)
+    }
 
 instance Monad m => Category (ComposePipe m) where
     id = ComposePipe $ maybe idP Pure
@@ -165,4 +253,41 @@ main = hspec $ do
         it "idP back" $
             runPipeI (sourceList [1 :: Int ..] >+> take 10 >+> consume >-> idP)
                 `shouldBe` [1..10]
+    describe "leftovers" $ do
+        it "works" $ do
+            let sink :: PipeL Int () () Identity [Int]
+                sink = do
+                    leftover (1 :: Int)
+                    pipeL (takeL 5 >> stopL) consumeL
+                --src :: PipeL () Int [Int] Identity [Int]
+                src = sourceListL [2..20] >> stopL
+                res = runPipeLI $ pipeL src sink
+            res `shouldBe` [1..5]
+        it "idL front" $ do
+            let sink :: PipeL Int () () Identity [Int]
+                sink = do
+                    leftover (1 :: Int)
+                    pipeL (takeL 5 >> stopL) consumeL
+                --src :: PipeL () Int [Int] Identity [Int]
+                src = sourceListL [2..20] >> stopL
+                res = runPipeLI $ pipeL idL (pipeL src sink)
+            res `shouldBe` [1..5]
+        it "idL middle" $ do
+            let sink :: PipeL Int () () Identity [Int]
+                sink = do
+                    leftover (1 :: Int)
+                    pipeL (takeL 5 >> stopL) consumeL
+                --src :: PipeL () Int [Int] Identity [Int]
+                src = sourceListL [2..20] >> stopL
+                res = runPipeLI $ pipeL src (pipeL idL sink)
+            res `shouldBe` [1..5]
+        it "idL back" $ do
+            let sink :: PipeL Int () () Identity [Int]
+                sink = do
+                    leftover (1 :: Int)
+                    pipeL (takeL 5 >> stopL) consumeL
+                --src :: PipeL () Int [Int] Identity [Int]
+                src = sourceListL [2..20] >> stopL
+                res = runPipeLI $ pipeL src (pipeL sink idL)
+            res `shouldBe` [1..5]
     return ()
